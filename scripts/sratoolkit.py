39ad38e7fc23dd7203d5c3a777dbd8723629e5c4
Umar Siddiqui <usiddiqui1124@gmail.com>
2025-03-21 17:11:58 -0500

    added comments

--- scripts/sratoolkit.py
+++ scripts/sratoolkit.py
@@ -2,18 +2,25 @@ import subprocess
 import Bio.SeqIO as SeqIO
 import os
 
-os.makedirs('../fastq-data',exist_ok=True)
+#create directory for fastq files 
+os.makedirs('../fastq-data',exist_ok=True) 
 fastq_filepath = '../fastq-data'
 
-SRR = 'SRR32805580'
+SRR = 'SRR32805580' #SRR chosen by user 
+
+#prefetch and fastq dump commands 
 download_sra = 'prefetch ' + SRR
 fastqdump = 'fasterq-dump ' + SRR + ' -O ' + fastq_filepath
 
+#run commands in the command line
 subprocess.run(download_sra, shell=True)
 subprocess.run(fastqdump, shell=True)
 
+#create a directory for spades output 
 os.makedirs('../spades-out',exist_ok=True)
 
+#spades assembly command 
 spades_cmd = 'spades.py -1 ../fastq-data/' + SRR + '_1.fastq -2 ../fastq-data/' + SRR + '_2.fastq -o ../spades-out' 
 
+#run spades command in the command line
 subprocess.run(spades_cmd,shell=True)
