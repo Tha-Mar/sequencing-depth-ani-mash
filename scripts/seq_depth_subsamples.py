0051f8e542285d485e08e1b387c5f1885192dea1
Lila Nelson <lnelson5452@gmail.com>
2025-03-23 12:43:17 -0500

    editing path to call seqtk LN

--- scripts/seq_depth_subsamples.py
+++ scripts/seq_depth_subsamples.py
@@ -13,8 +13,8 @@ for i in range(int(max_cov/step)):
     read1 = str(round(cov/max_cov*len(seqs1)))
     read2 = str(round(cov/max_cov*len(seqs2)))
     for j in range(5): # ask about when i move this into the repo how I need to change calling seqtk 
-        command1 = 'seqtk/seqtk sample -s100 '+SRR+'_1.fastq '+read1+' > test_cov_depths/ecoli_'+str(cov)+'_'+str(j)+'_1.fastq' # will probably want to change ecoli to the SRR number so it can change 
-        command2 = 'seqtk/seqtk sample -s100 '+SRR+'_2.fastq '+read1+' > test_cov_depths/ecoli_'+str(cov)+'_'+str(j)+'_2.fastq'
+        command1 = '../../seqtk/seqtk sample -s100 '+SRR+'_1.fastq '+read1+' > test_cov_depths/ecoli_'+str(cov)+'_'+str(j)+'_1.fastq' # will probably want to change ecoli to the SRR number so it can change 
+        command2 = '../../seqtk/seqtk sample -s100 '+SRR+'_2.fastq '+read1+' > test_cov_depths/ecoli_'+str(cov)+'_'+str(j)+'_2.fastq'
         os.system(command1)
         os.system(command2)
     cov += step
