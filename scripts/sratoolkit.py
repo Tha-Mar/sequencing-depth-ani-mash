a936381c858b3e285b4a2c8e99cd7a095956e990
Jerrin John <jjohn7@luc.edu>
2025-03-24 15:59:17 -0500

    updated script to take infile

--- scripts/sratoolkit.py
+++ scripts/sratoolkit.py
@@ -1,12 +1,24 @@
 import subprocess 
 import Bio.SeqIO as SeqIO
 import os
+import argparse
+import sys
+
+#function to parse command line arguments
+def check_arg(args=None):
+    parser = argparse.ArgumentParser(description='input location of data directory')
+    parser.add_argument('-i', '--input', help='path to input data',required ='True') #add command line arguement for input file
+    return parser.parse_args(args)
+
+#retrieve command line arguments and assign to variables
+args = check_arg(sys.argv[1:])
+infile = args.input #store input file path
 
 #create directory for fastq files 
 os.makedirs('../fastq-data',exist_ok=True) 
 fastq_filepath = '../fastq-data'
 
-SRR = 'SRR32805580' #SRR chosen by user 
+SRR = infile
 
 #prefetch and fastq dump commands 
 download_sra = 'prefetch ' + SRR
@@ -24,3 +36,4 @@ spades_cmd = 'spades.py -1 ../fastq-data/' + SRR + '_1.fastq -2 ../fastq-data/' 
 
 #run spades command in the command line
 subprocess.run(spades_cmd,shell=True)
+
