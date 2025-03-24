02b2aafea98c2810d91fed8ea87100f96f444f2c
Lila Nelson <lnelson5452@gmail.com>
2025-03-24 15:27:00 -0500

    changing SRR number for testing

--- scripts/seq_depth_subsamples.py
+++ scripts/seq_depth_subsamples.py
@@ -1,13 +1,17 @@
 from Bio import SeqIO
 import os
 
+os.system('cut -f 3 ~/coverage_table > depths')
+with open('~/depths', 'r') as f:
+    depths = f.readlines()
+    max_cov = max(depths)
 # max_cov = will have to get it from samtools coverage from bam file from assembly 
-max_cov = 10 # need to write a command using samtools to get the full coverage 
+#max_cov = 10 # need to write a command using samtools to get the full coverage 
 step = 5
 cov = 5
-SRR = 'SRR13921543' # can change this to infile once we are further into the process
-seqs1 = list(SeqIO.parse(SRR+'_1.fastq', 'fastq'))
-seqs2 = list(SeqIO.parse(SRR+'_2.fastq', 'fastq'))
+SRR = 'SRR32805580' # can change this to infile once we are further into the process
+seqs1 = list(SeqIO.parse('~/fastq-data'+SRR+'_1.fastq', 'fastq'))
+seqs2 = list(SeqIO.parse('~/fastq-data'+SRR+'_2.fastq', 'fastq'))
 
 for i in range(int(max_cov/step)):
     read1 = str(round(cov/max_cov*len(seqs1)))
