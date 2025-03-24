4e831bbf8c3a3501c9bb39d5ebd08805fb00a912
Lila Nelson <lnelson5452@gmail.com>
2025-03-24 16:50:33 -0500

    fixed paths and added argparse

--- scripts/bt2.py
+++ scripts/bt2.py
@@ -1,32 +1,30 @@
 import os
-# below I use the argparse function from the start_argparse.py file we were given
-
-'''import sys
 import argparse
+import sys
 
 #function to parse command line arguments
 def check_arg(args=None):
-    parser = argparse.ArgumentParser(description="ADD TITLE OF SCRIPT HERE (shows on help)")
-    parser.add_argument("-i", "--input",
-    help="input file",
-    required=True)
-    parser.add_argument("-o", "--output",
-    help="output file",
-    required=True)
+    parser = argparse.ArgumentParser(description='input location of data directory')
+    parser.add_argument('-i', '--input', help='path to input data',required ='True') #add command line arguement for input file
     return parser.parse_args(args)
 
-#retrieve command line arguments
-arguments = check_arg(sys.argv[1:])
-infile = arguments.input
-outfile = arguments.output'''
+#retrieve command line arguments and assign to variables
+args = check_arg(sys.argv[1:])
+infile = args.input #store input file path
+
+#create directory for fastq files 
+os.makedirs('../fastq-data',exist_ok=True) 
+fastq_filepath = '../fastq-data'
+
+SRR = infile
 
 os.system('mkdir ../bt2')
-bowtie2_index = 'bowtie2-build ~/spades-out/contigs.fasta ~/bt2/bt2_index'
-bowtie2_cmd = 'bowtie2 -x ~/bt2/bt2_index -1 ~/fastq-data/SRR32805580_1.fastq -2 ~/fastq-data/SRR32805580_2.fastq -S ~/bt2/SRR32805580.sam'
+bowtie2_index = 'bowtie2-build ../spades-out/contigs.fasta ~/bt2/bt2_index'
+bowtie2_cmd = 'bowtie2 -x ../bt2/bt2_index -1 ../fastq-data/'+SRR+'_1.fastq -2 ../fastq-data/'+SRR+'_2.fastq -S ../bt2/'+SRR+'.sam'
 
-samtools_view = 'samtools view -b ~/bt2/SRR32805580.sam > ~/bt2/SRR32805580.bam'
-samtools_sort = 'samtools sort ~/bt2/SRR32805580.bam -o ~/bt2/sorted_SRR32805580.bam'
-samtools_cmd = 'samtools depth ~/bt2/sorted_SRR32805580.bam -o ~/coverage_table '
+samtools_view = 'samtools view -b ../bt2/'+SRR+'.sam > ../bt2/'+SRR+'.bam'
+samtools_sort = 'samtools sort ../bt2/'+SRR+'.bam -o ../bt2/sorted_'+SRR+'.bam'
+samtools_cmd = 'samtools depth ../bt2/sorted_'+SRR+'.bam -o ../coverage_table '
 
 os.system(bowtie2_index)
 os.system(bowtie2_cmd)
