0d2d1de36465eb99ca46b535a250467cd9378ec3
Jerrin John <jjohn7@luc.edu>
2025-03-25 12:20:21 -0500

    added code for qc

--- scripts/sratoolkit.py
+++ scripts/sratoolkit.py
@@ -24,15 +24,33 @@ SRR = infile
 download_sra = 'prefetch ' + SRR
 fastqdump = 'fasterq-dump ' + SRR + ' -O ' + fastq_filepath
 
+
 #run commands in the command line
 subprocess.run(download_sra, shell=True)
 subprocess.run(fastqdump, shell=True)
+
+#QC the reads commands
+cutadapt1 = 'cutadapt -q 20 -o ../fastq-data/' + SRR + '_1_trimmed.fastq ../fastq-data/'  + SRR + '_1.fastq'
+FastQC1 = 'fastqc ../fastq-data/' + SRR + '_1_trimmed.fastq -o ../fastqc-out -f fastq'
+cutadapt2 = 'cutadapt -q 20 -o ../fastq-data/' + SRR + '_2_trimmed.fastq ../fastq-data/'  + SRR + '_2.fastq'
+FastQC2 = 'fastqc ../fastq-data/' + SRR + '_2_trimmed.fastq -o ../fastqc-out -f fastq'
+
+
+#Create directory to store FastQC files
+os.makedirs('../fastqc-out',exist_ok=True)
+
+#Run QC commands
+subprocess.run(cutadapt1, shell=True)
+subprocess.run(FastQC1, shell=True)
+subprocess.run(cutadapt2, shell=True)
+subprocess.run(FastQC2, shell=True)
+
 
 #create a directory for spades output 
 os.makedirs('../spades-out',exist_ok=True)
 
 #spades assembly command 
-spades_cmd = 'spades.py -1 ../fastq-data/' + SRR + '_1.fastq -2 ../fastq-data/' + SRR + '_2.fastq -o ../spades-out' 
+spades_cmd = 'spades.py -1 ../fastq-data/' + SRR + '_1_trimmed.fastq -2 ../fastq-data/' + SRR + '_2_trimmed.fastq -o ../spades-out' 
 
 #run spades command in the command line
 subprocess.run(spades_cmd,shell=True)
