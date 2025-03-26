dfdc1689fef3cbba114d1abed71bb9bf053f720a
Lila Nelson <lnelson5452@gmail.com>
2025-03-26 11:34:51 -0500

    script to assemble the subsampled reads

---
+++ scripts/assembly.py
@@ -0,0 +1,23 @@
+import os
+import glob
+import argparse
+import sys
+
+#function to parse command line arguments
+def check_arg(args=None):
+    parser = argparse.ArgumentParser(description='input location of data directory')
+    parser.add_argument('-i', '--input', help='path to input data',required ='True') #add command line arguement for input file
+    return parser.parse_args(args)
+
+#retrieve command line arguments and assign to variables
+args = check_arg(sys.argv[1:])
+infile = args.input #store input file path
+
+SRR = infile
+
+reads = glob.glob('../sub_samples_trimmed/'+SRR+'_20_*_1_trimmed.fastq')
+
+for read in reads:
+    file = os.path.basename(read)
+    spades_cmd = 'spades.py -1 '+read+' -2 '+read[:-15]+'2_trimmed.fastq -o ../sub_assemblies/'+file[:-16]
+    os.system(spades_cmd)
