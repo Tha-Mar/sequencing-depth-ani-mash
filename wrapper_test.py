9bdc77322752a277ab2fc42bb5b463189ae476c3
Jerrin John <jjohn7@luc.edu>
2025-04-03 09:27:10 -0500

    new wrapper script for test run

---
+++ wrapper_test.py
@@ -0,0 +1,11 @@
+import os 
+
+
+SRR = "SRR32805580"
+
+#Run script to download SRR fastq files and assemble with SPAdes
+os.system(f"python ./scripts/test_SRA.py --input {SRR}")
+os.system(f'python ./scripts/test_subsample.py --input {SRR}')
+os.system(f'python ./scripts/assembly.py --input {SRR}')
+os.system(f'python ./scripts/fastani-mash.py --input {SRR}')
+
