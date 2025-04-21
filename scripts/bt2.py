b7b35c3bee5632debd282c64319100265e3e2622
Lila Nelson <lnelson5452@gmail.com>
2025-04-21 14:33:21 -0500

    commenting my code

--- scripts/bt2.py
+++ scripts/bt2.py
@@ -12,6 +12,7 @@ def check_arg(args=None):
 args = check_arg(sys.argv[1:])
 infile = args.input #store input file path
 
+# setting the SRR accession number so this functions for any input
 SRR = infile
 
 #create directory for fastq files 
@@ -19,15 +20,19 @@ os.makedirs('../fastq-data-'+SRR,exist_ok=True)
 fastq_filepath = '../fastq-data-'+SRR
 
 
-
+# creating a directory for bowtie2 output
 os.system('mkdir ../bt2-'+SRR)
+
+# creating the commands for building the bowtie2 index and for using that index to map the reads
 bowtie2_index = 'bowtie2-build ../spades-out-'+SRR+'/contigs.fasta ../bt2-'+SRR+'/bt2_index'
 bowtie2_cmd = 'bowtie2 -x ../bt2-'+SRR+'/bt2_index -1 ../fastq-data-'+SRR+'/'+SRR+'_1_trimmed.fastq -2 ../fastq-data-'+SRR+'/'+SRR+'_2_trimmed.fastq -S ../bt2-'+SRR+'/'+SRR+'.sam'
 
+# creating the commands to take the .sam file from bt2 convert it to a .bam, sort that .bam file, and calcuate the depth
 samtools_view = 'samtools view -b ../bt2-'+SRR+'/'+SRR+'.sam > ../bt2-'+SRR+'/'+SRR+'.bam'
 samtools_sort = 'samtools sort ../bt2-'+SRR+'/'+SRR+'.bam -o ../bt2-'+SRR+'/sorted_'+SRR+'.bam'
 samtools_cmd = 'samtools depth ../bt2-'+SRR+'/sorted_'+SRR+'.bam -o ../coverage_table-'+SRR
 
+# calling each of the functions in order that were created above
 os.system(bowtie2_index)
 os.system(bowtie2_cmd)
 os.system(samtools_view)
